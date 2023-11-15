64cf7a0774a68ebece450e379dc6fbfdd426b130
Hoang Nguyen <51772253+hoanggtg@users.noreply.github.com>
2023-11-15 11:53:51 +0700

    update js

---
+++ js/tinh-tuoi.js
@@ -0,0 +1,66 @@
+let countError = 0;
+let countSuccess = 0;
+
+function resetCount() {
+  countSuccess = 0;
+  countError = 0;
+}
+
+function setMessage(text) {
+  document.getElementById('message').innerHTML = text;
+}
+
+function ageCalculate() {
+  let age = document.getElementById('input-age').value;
+
+  if (document.getElementById('input-age').value.length === 0 || isNaN(age)) {
+    if (document.getElementById('input-age').value.length === 0) {
+      setMessage('Chưa nhập gì hết.');
+    } else {
+      countError++;
+
+      switch (countError) {
+        case 0:
+          setMessage(age + ' là tuổi clgt???');
+          break;
+        case 1:
+          setMessage('Đây là lần thứ 2 rồi nhưng bạn vẫn nhập sai.');
+          break;
+        case 2:
+          setMessage(
+            'Thật ra bạn chỉ bấm cái nút này cho vui thôi phải không?'
+          );
+          break;
+        case 3:
+        case 4:
+        case 5:
+          for (let i = 0; i <= countError; i++) {
+            setMessage('đừng có nhấn nữa '.repeat(i));
+          }
+          break;
+        case 6:
+          setMessage('nhấn thêm lần nữa xem?');
+          break;
+        case 7:
+          setMessage('chắc chưa?');
+          document.getElementById('btn-submit').innerHTML = 'Chắc';
+          break;
+
+        default:
+          // các case còn lại (>7)
+          window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+      }
+    }
+  } else {
+    countSuccess++;
+
+    if (countSuccess === 1) {
+      setMessage('Bạn đã ' + age + ' tuổi rồi.');
+      countError = 0;
+    } else {
+      setMessage(
+        `Bạn đã nhấn cái nút này ${countSuccess} lần, tuổi của bạn vẫn là ${age}`
+      );
+    }
+  }
+}
