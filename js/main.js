45a0350bb5ff1c1adc7939ee0e2f6ff33ba0e814
Hoang Nguyen <51772253+hoanggtg@users.noreply.github.com>
2023-11-15 17:40:41 +0700

    update content

--- js/main.js
+++ js/main.js
@@ -1,20 +1,37 @@
-function openCity(evt, cityName) {
-  // Declare all variables
-  let i, tabcontent, tablinks;
+function openTab(event, tabId) {
+  // Get all elements with class="tab-menu" and remove the class "active"
+  removeActiveTab('tab-menu');
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName('tabcontent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
+  // Get all elements with class="tab-content" and remove the class "active"
+  removeActiveTab('tab-content');
+
+  // Show the current tab, and add an "active" class
+  event.currentTarget.className += ' active';
+  document.getElementById(tabId).className += ' active';
+
+  if (tabId === 'Khunglong') {
+    initDinoGame();
   }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName('tablinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = 'block';
-  evt.currentTarget.className += ' active';
 }
+
+function initDinoGame() {
+  let container = document.getElementById('Khunglong');
+  let content = container.innerHTML;
+  container.innerHTML = content;
+
+  let dinoFrame = document.getElementById('dino-frame');
+  dinoFrame.focus();
+}
+
+function removeActiveTab(className) {
+  let elements = document.getElementsByClassName(className);
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.remove('active');
+  }
+}
+
+window.addEventListener('keydown', function (e) {
+  if (e.code === 'Space' && e.target == document.body) {
+    e.preventDefault();
+  }
+});
