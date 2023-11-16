246c4d598d0eeb0402ba236b50e4c12144e1a4f6
Anh Hoang <anhzpt3@gmail.com>
2023-11-16 16:37:33 +0700

    tower_blocks

--- js/main.js
+++ js/main.js
@@ -35,3 +35,19 @@ window.addEventListener('keydown', function (e) {
     e.preventDefault();
   }
 });
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
